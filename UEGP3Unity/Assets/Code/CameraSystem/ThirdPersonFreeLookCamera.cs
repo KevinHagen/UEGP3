91b7e6a0611e1d06c42b7b421933025764264a11
Kevin Hagen <kevin.hagen@ue-germany.de>
2020-04-14 18:21:10 +0200

    ADD basic camera orbiting with gimbal lock problem

--- UEGP3Unity/Assets/Code/CameraSystem/ThirdPersonFreeLookCamera.cs
+++ UEGP3Unity/Assets/Code/CameraSystem/ThirdPersonFreeLookCamera.cs
@@ -10,6 +10,12 @@ public class ThirdPersonFreeLookCamera : MonoBehaviour
 	[SerializeField]
 	private Transform _cameraPivot;
 
+	[Header("Camera Speed Settings")]
+	[Tooltip("The speed the camera uses to rotate around the x-axis")]
+	[SerializeField] private float _pitchSpeed = 100f;
+	[Tooltip("The speed the camera uses to rotate around the y-axis")]
+	[SerializeField] private float _yawSpeed = 100f;
+	
 	// Default offset as configured in the scene view
 	private Vector3 _defaultPosition;
 	
@@ -23,7 +29,13 @@ public class ThirdPersonFreeLookCamera : MonoBehaviour
 	// This helps avoiding jittery movement.
 	private void LateUpdate()
 	{
+		// Fetch inputs from the mouse, multiply by Time.deltaTime so its frame-rate independent
+		float deltaYaw = Input.GetAxis("Mouse X") * Time.deltaTime;
+		float deltaPitch = Input.GetAxis("Mouse Y") * Time.deltaTime;
+		
+		// Rotate by the movement delta * speed to receive camera angle
+		transform.eulerAngles += new Vector3(deltaPitch * _pitchSpeed, deltaYaw * _yawSpeed, 0);
 		//  Most basic use-case: A follower camera where the camera maintains the initial position each frame.
-		transform.position = _cameraPivot.position - _defaultPosition;
+		transform.position = _cameraPivot.position - transform.forward * 4.5f;
 	}
 }
