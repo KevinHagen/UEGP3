8063125162da810781515ebf9551e83afd90c72d
Kevin Hagen <kevin.hagen@ue-germany.de>
2020-05-05 17:38:43 +0200

    ADD inventory and item logic

---
+++ UEGP3Unity/Assets/Code/InventorySystem/Inventory.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UEGP3.InventorySystem
+{
+	[CreateAssetMenu(menuName = "UEGP3/Inventory System/New Inventory", fileName = "New Inventory")]
+	public class Inventory : ScriptableObject
+	{
+		private Dictionary<Item, int> _inventoryItems = new Dictionary<Item, int>();
+
+		/// <summary>
+		/// Prints the inventory to the console.
+		/// </summary>
+		public void ShowInventory()
+		{
+			Debug.Log(this);
+		}
+		
+		public bool TryAddItem(Item item)
+		{
+			bool success = false;
+
+			// Item is not yet in inventory, add it
+			if (!_inventoryItems.ContainsKey(item))
+			{
+				_inventoryItems.Add(item, 1);
+				success = true;
+			}
+			// Item is already in inventory, stack it up if possible
+			else
+			{
+				// Only items that are not unique can be stacked
+				if (!item.IsUnique)
+				{
+					_inventoryItems[item]++;
+					success = true;
+				}
+			}
+			return success;
+		}
+
+		public override string ToString()
+		{
+			// "String-Interpolation": $ before a string "" allows us to use variables in {} 
+			// inventory = "Inventory " + name + " contains:\r\n" is the same as the line below, but nicer! :) 
+			string inventory = $"Inventory {name} contains:\r\n";
+
+			foreach (KeyValuePair<Item,int> inventoryItem in _inventoryItems)
+			{
+				inventory += $"[{inventoryItem.Key.ItemName} - {inventoryItem.Value}]\r\n";
+			}
+			
+			return inventory;
+		}
+	}
+}
---
+++ UEGP3Unity/Assets/Code/InventorySystem/Item.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace UEGP3.InventorySystem
+{
+	[CreateAssetMenu(fileName = "New Item", menuName = "UEGP3/Inventory System/New Item")]
+	public class Item : ScriptableObject
+	{
+		[Tooltip("The name of the item")] [SerializeField]
+		private string _itemName;
+		[Tooltip("Short description of the item, shown to the player")] [SerializeField]
+		private string _description;
+		[Tooltip("A small icon of the item")] [SerializeField]
+		private Sprite _itemSprite;
+		[Tooltip("Is the item being consumed after usage?")] [SerializeField]
+		private bool _consumeUponUse;
+		[Tooltip("A unique item can not be stacked in the players inventory")] [SerializeField]
+		private bool _isUnique;
+		
+		/* // C# Auto-Property
+		public bool ConsumeUponuse { get; set; }
+		// C# Property: We can define more logic in get & set
+		public Sprite ItemSprite {
+			get
+			{
+				if (_isUnique)
+				{
+					return _uniqueSprite;
+				}
+				else
+				{
+					return _normalSprite;
+				}
+			}
+			set
+			{
+				if (_consumeUponUse)
+				{
+					// Do some more logic
+				}
+				_itemSprite = value;
+			}
+		}
+		*/
+		
+		// public getter only - "readonly"
+		public bool IsUnique => _isUnique;
+		public string ItemName => _itemName;
+
+		/// <summary>
+		/// Uses the item and executes its effect.
+		/// </summary>
+		public void UseItem()
+		{
+			Debug.Log($"Using item {_itemName}");
+		}
+	}
+}
