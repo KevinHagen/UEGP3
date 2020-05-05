9b2a4879835fa1a7d26f934dfb2e35b1d7797f32
Kevin Hagen <kevin.hagen@ue-germany.de>
2020-05-05 18:04:58 +0200

    ADD item pick up logic

---
+++ UEGP3Unity/Assets/Code/InventorySystem/ItemPickup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UEGP3.Core;
+using UnityEngine;
+
+namespace UEGP3.InventorySystem
+{
+	[RequireComponent(typeof(SphereCollider))]
+	public class ItemPickup : MonoBehaviour, ICollectible
+	{
+		[Tooltip("The scriptable object of the item to be picked up.")] [SerializeField]
+		private Item _itemToPickup;
+		[Tooltip("Range in which the pick up is performed.")] [SerializeField] 
+		private float _pickupRange;
+		
+		private SphereCollider _pickupCollider;
+
+		private void Awake()
+		{
+			// Get collider on same object
+			_pickupCollider = GetComponent<SphereCollider>();
+			
+			// Ensure collider values are set accordingly
+			_pickupCollider.radius = _pickupRange;
+			_pickupCollider.isTrigger = true;
+		}
+
+		public void Collect(Inventory inventory)
+		{
+			// Add item to inventory
+			bool wasPickedUp = inventory.TryAddItem(_itemToPickup);
+
+			// Destroy the pickup once the object has been successfully picked up
+			if (wasPickedUp)
+			{
+				Destroy(gameObject);
+			}
+		}
+		
+#if UNITY_EDITOR
+		private void OnValidate()
+		{
+			if (!_pickupCollider)
+			{
+				_pickupCollider = GetComponent<SphereCollider>();
+			}
+			
+			// Ensure radius is set correctly
+			_pickupCollider.radius = _pickupRange;
+		}
+#endif
+	}
+}
---
+++ UEGP3Unity/Assets/Code/PlayerSystem/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UEGP3.Core;
+using UEGP3.InventorySystem;
+using UnityEngine;
+
+namespace UEGP3.PlayerSystem
+{
+	/// <summary>
+	/// Player containing additional logic like item pick ups etc.
+	/// </summary>
+	public class Player : MonoBehaviour
+	{
+		[Tooltip("Inventory to be used for the player")] [SerializeField] 
+		private Inventory _playerInventory;
+
+		private void Update()
+		{
+			// Show Inventory if button is pressed
+			if (Input.GetButtonDown("Inventory"))
+			{
+				_playerInventory.ShowInventory();
+			}
+		}
+
+		private void OnTriggerEnter(Collider other)
+		{
+			// If we collide with a collectible item, collect it
+			ICollectible collectible = other.gameObject.GetComponent<ICollectible>();
+			if (collectible != null)
+			{
+				Collect(collectible);
+			}
+		}
+
+		private void Collect(ICollectible collectible)
+		{
+			collectible.Collect(_playerInventory);
+		}
+	}
+}
