3ec1118cb35fc6b98b03829f8093bf51801b4da6
Kevin Hagen <kvn.hagen@gmail.com>
2020-06-09 18:43:45 +0200

    Add method stubs to GenerateEventFunctionsDemoClass.cs

--- UEGP3Unity/Assets/Code/Demos/Rider/GenerateEventFunctionsDemoClass.cs
+++ UEGP3Unity/Assets/Code/Demos/Rider/GenerateEventFunctionsDemoClass.cs
@@ -1,8 +1,38 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace UEGP3.Demos.Rider
 {
 	public class GenerateEventFunctionsDemoClass : MonoBehaviour
 	{
+		private void Awake()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnDrawGizmos()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnDrawGizmosSelected()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnMouseUp()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void Start()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void Update()
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
