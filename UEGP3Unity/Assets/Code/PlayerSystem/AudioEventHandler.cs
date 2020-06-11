82a81d5cc6ffb3c4dfaf0bc8f5b0d304b5cb3bcd
Kevin Hagen <kvn.hagen@gmail.com>
2020-06-11 19:24:12 +0200

    CHANGE refactored FootStepAudioHandler to AudioEventHandler (keep old one for comparison)

---
+++ UEGP3Unity/Assets/Code/PlayerSystem/AudioEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using UEGP3.Core;
+using UnityEngine;
+
+namespace UEGP3.PlayerSystem
+{
+	[RequireComponent(typeof(AudioSource))]
+	public class AudioEventHandler : MonoBehaviour
+	{
+		private AudioSource _audioSource;
+
+		private void Awake()
+		{
+			_audioSource = GetComponent<AudioSource>();
+		}
+
+		// Called as an animation event
+		private void PlayAudioEvent(AnimationEvent animEvent)
+		{
+			ScriptableAudioEvent audioEvent = animEvent.objectReferenceParameter as ScriptableAudioEvent;
+
+			if (audioEvent != null)
+			{
+				audioEvent.Play(_audioSource);
+			}
+		}
+	}
+}
