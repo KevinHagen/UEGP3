957f7b50992c14b6fb248843a1bb483d38f3d664
Kevin Hagen <kvn.hagen@gmail.com>
2020-06-16 19:15:04 +0200

    ADD Random Number Generation Tests

---
+++ UEGP3Unity/Assets/Code/Demos/PCGDemos/SimpleRNGTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+namespace UEGP3.Demos.PCGDemos
+{
+    public class SimpleRNGTest : MonoBehaviour
+    {
+        [Header("Generation")] [SerializeField] [Tooltip("Text that display our random number")]
+        private TextMeshProUGUI _randomNumberText;
+        [SerializeField] [Tooltip("Button to generate a new random number")]
+        private Button _randomNumberButton;
+
+        [Header("Seed & PRNG")] [SerializeField] [Tooltip("Seed used to generate a random number")]
+        private int _seed;
+        [SerializeField] [Tooltip("Button to re-init the prng with the configured seed")]
+        private Button _initPRNGButton;
+
+        private void Awake()
+        {
+            _randomNumberButton.onClick.AddListener(GenerateRandomNumber);
+            _initPRNGButton.onClick.AddListener(ResetToSeed);
+            ResetToSeed();
+        }
+
+        private void GenerateRandomNumber()
+        {
+            _randomNumberText.text = (Random.value * 10f).ToString("0.00");
+        }
+
+        private void ResetToSeed()
+        {
+            Random.InitState(_seed);
+            GenerateRandomNumber();
+        }
+    }
+}
---
+++ UEGP3Unity/Assets/Code/Demos/PCGDemos/SimpleRNGTestSystem.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UEGP3.Demos.PCGDemos.RNGTests
+{
+	public class SimpleRNGTestSystem : MonoBehaviour
+	{
+		[Header("Generation")]
+		[SerializeField]
+		[Tooltip("Text that displays our random number")]
+		private TextMeshProUGUI _randomNumberText;
+		[SerializeField] [Tooltip("Button to generate a new random number")]
+		Button _generateNumberButton;
+
+		[Header("Seed & PRNG")]
+		[SerializeField] 
+		[Tooltip("Seed used to generate a random number")]
+		private int _seed;
+		[SerializeField] [Tooltip("Button used to re-init the PRNG with the configured value")]
+		private Button _initPRNGButton;
+
+		private System.Random _randomNumberGenerator;
+		
+		private void Awake()
+		{
+			_generateNumberButton.onClick.AddListener(GenerateRandomNumber);
+			_initPRNGButton.onClick.AddListener(ResetToSeed);
+			ResetToSeed();
+		}
+
+		private void GenerateRandomNumber()
+		{
+			_randomNumberText.text = (_randomNumberGenerator.NextDouble() * 10).ToString("0.00");
+		}
+
+		private void ResetToSeed()
+		{
+			_randomNumberGenerator = new System.Random(_seed);
+			GenerateRandomNumber();
+		}
+	}
+}
