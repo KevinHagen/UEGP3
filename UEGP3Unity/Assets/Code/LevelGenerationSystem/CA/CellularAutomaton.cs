ac823490e6c8618943438717f815f3ca4bb8e5a5
Kevin Hagen <kvn.hagen@gmail.com>
2020-06-23 19:09:25 +0200

    ADD Cellular Automaton basics

---
+++ UEGP3Unity/Assets/Code/LevelGenerationSystem/CA/CellularAutomaton.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using UnityEngine;
+using Random = System.Random;
+
+namespace UEGP3.LevelGenerationSystem.CA
+{
+	public class CellularAutomaton : MonoBehaviour
+	{
+		[Header("PRNG")] [SerializeField] [Tooltip("If set to true, the seed below will be used. Else it uses the default seed")]
+		private bool _useSeed;
+		[SerializeField] [Tooltip("Seed to use for the generation")]
+		private int _seed;
+		[Tooltip("Chance for a cell to start out alive")] [SerializeField] [Range(0, 1)]
+		private float _startAliveChance;
+		[SerializeField] [Tooltip("Reference to the gameboard which displays the result of the automaton")]
+		private GameBoard _gameBoard;
+		[SerializeField] [Tooltip("Number of generations used to smooth the CA result")] 
+		private int _generations;
+		private Random _rng;
+		private int _currentGeneration;
+		private bool[][,] _generationSteps;
+
+		private void Start()
+		{
+			// Init the automaton
+			InitAutomaton();
+			// Start the generation process
+			Generate();
+		}
+
+		private void Update()
+		{
+			// If LMB is pressed, generate a new start configuration
+			if (Input.GetMouseButtonDown(0))
+			{
+				InitAutomaton();
+				Generate();
+			}
+		}
+
+		private void Generate()
+		{
+			// need to retrieve next generation
+			CalculateNextGeneration();
+		}
+
+		private void CalculateNextGeneration()
+		{
+			bool[,] nextGeneration = new bool[_gameBoard.Width, _gameBoard.Height];
+
+			for (int x = 1; x < _gameBoard.Width - 1; x++)
+			{
+				for (int y = 1; y < _gameBoard.Height - 1; y++)
+				{
+					int surroundWalls = GetSurroundingWallsCount(x, y);
+					
+					// is a wall and has >= 4 neighbouring walls --> wall
+					
+					// is ground and has >= 5 neighbouring walls --> wall
+					
+					// otherwise its a ground tile
+				}
+			}
+			
+			// after the rules have been successfully applied, we can increase current generation and safe the generation as the current one
+			_currentGeneration++;
+			_generationSteps[_currentGeneration] = nextGeneration;
+		}
+
+		private int GetSurroundingWallsCount(int x, int y)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void InitAutomaton()
+		{
+			// store the current generation and set it to 0
+			_currentGeneration = 0;
+			
+			// store current generation and its configuration
+			_generationSteps = new bool[_generations + 1][,];
+			for (int i = 0; i < _generationSteps.Length; i++)
+			{
+				_generationSteps[i] = new bool[_gameBoard.Width, _gameBoard.Height];
+			}
+
+			// init prng
+			InitPRNG();
+
+			// init random distribution
+			InitRandomDistribution();
+			_gameBoard.PlotData(_generationSteps[_currentGeneration]);
+		}
+
+		private void InitRandomDistribution()
+		{
+			for (int x = 0; x < _gameBoard.Width; x++)
+			{
+				for (int y = 0; y < _gameBoard.Height; y++)
+				{
+					// border tiles should be walls by default
+					if ((x == 0) || (x == _gameBoard.Width - 1) || (y == 0) || (y == _gameBoard.Height - 1))
+					{
+						_generationSteps[0][x, y] = false;
+					}
+					// others should be randomly chosen
+					else
+					{
+						_generationSteps[0][x, y] = _rng.NextDouble() < _startAliveChance;
+					}
+				}
+			}
+		}
+
+		private void InitPRNG()
+		{
+			_rng = _useSeed ? new Random(_seed) : new Random();
+		}
+	}
+}
---
+++ UEGP3Unity/Assets/Code/LevelGenerationSystem/CA/GameBoard.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+namespace UEGP3.LevelGenerationSystem.CA
+{
+	public class GameBoard : MonoBehaviour
+	{
+		[SerializeField] [Tooltip("Width of the board")]
+		private int _width;
+		[SerializeField] [Tooltip("Height of the board")]
+		private int _height;
+		[SerializeField] [Tooltip("Prefab used to generate the whole board.")]
+		private SpriteRenderer _groundPrefab;
+
+		// 2d array to store all tiles on the gameboard
+		private SpriteRenderer[,] _tiles;
+		// example to show how a "jagged" array looks like
+		private SpriteRenderer[][] _otherTiles;
+		
+		public int Width => _width;
+		public int Height => _height;
+
+		private void Awake()
+		{
+			SpawnMap();
+		}
+
+		private void SpawnMap()
+		{
+			_tiles = new SpriteRenderer[_width, _height];
+			for (int x = 0; x < _width; x++)
+			{
+				for (int y = 0; y < _height; y++)
+				{
+					_tiles[x, y] = Instantiate(_groundPrefab, gameObject.transform);
+					_tiles[x, y].transform.position = new Vector3(x, y);
+					// All tiles start as walls, which are represented by the color black
+					_tiles[x, y].color = Color.black;
+				}
+			}
+			
+			// _otherTiles = new SpriteRenderer[5][];
+			// _otherTiles[1] = new SpriteRenderer[3];
+			// _otherTiles[2] = new SpriteRenderer[10];
+		}
+
+		public void PlotData(bool[,] tileData)
+		{
+			for (int x = 0; x < _width; x++)
+			{
+				for (int y = 0; y < _height; y++)
+				{
+					_tiles[x, y].color = tileData[x, y] ? Color.white : Color.black;
+				}
+			}
+		}
+	}
+}
